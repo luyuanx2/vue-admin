8f4951adfaa172d7cbf8f224e13ad19a7941319f
luyuanyuan <694436921@qq.com>
2018-01-23 00:33:27 +0800

    提交

--- src/components/login/index.vue
+++ src/components/login/index.vue
@@ -1,45 +1,65 @@
 <template>
   <div class="login-container">
-    <!--<el-form autoComplete="on" :model="loginForm" :rules="loginRules" ref="loginForm" label-position="left"-->
-             <!--label-width="0px"-->
-             <!--class="card-box login-form">-->
-      <!--<h3 class="title">vue-element-admin</h3>-->
-      <!--<el-form-item prop="username">-->
-        <!--<span class="svg-container svg-container_login">-->
-          <!--<svg-icon icon-class="user"/>-->
-        <!--</span>-->
-        <!--<el-input name="username" type="text" v-model="loginForm.username" autoComplete="on" placeholder="username"/>-->
-      <!--</el-form-item>-->
-      <!--<el-form-item prop="password">-->
-        <!--<span class="svg-container">-->
-          <!--<svg-icon icon-class="password"></svg-icon>-->
-        <!--</span>-->
-        <!--<el-input name="password" :type="pwdType" @keyup.enter.native="handleLogin" v-model="loginForm.password"-->
-                  <!--autoComplete="on"-->
-                  <!--placeholder="password"></el-input>-->
-        <!--<span class="show-pwd" @click="showPwd"><svg-icon icon-class="eye"/></span>-->
-      <!--</el-form-item>-->
-      <!--<el-form-item>-->
-        <!--<el-button type="primary" style="width:100%;" :loading="loading" @click.native.prevent="handleLogin">-->
-          <!--Sign in-->
-        <!--</el-button>-->
-      <!--</el-form-item>-->
-      <!--<div class="tips">-->
-        <!--<span style="margin-right:20px;">username: admin</span>-->
-        <!--</span> password: admin</span>-->
-      <!--</div>-->
-      <!--<el-button class="thirdparty-button" type="primary" @click="showDialog=true">第三方登录</el-button>-->
-    <!--</el-form>-->
-    <div style="width: 420px;height:450px; position: absolute;
+    <el-form  autoComplete="on" :model="loginForm" :rules="loginRules" ref="loginForm" label-position="left"
+              label-width="0px"
+              class="card-box login-form">
+      <h3 class="title">后台管理系统</h3>
+      <el-tabs v-model="activeName" @tab-click="handleClick">
+        <el-tab-pane label="账户密码登录" name="first">
+          <el-form-item prop="qwe">
+            <el-input
+              placeholder="用户名"
+              prefix-icon="el-icon-search">
+            </el-input>
+          </el-form-item>
+          <el-form-item prop="rty">
+            <el-input
+              placeholder="密码"
+              prefix-icon="el-icon-search">
+            </el-input>
+            <span class="show-pwd" @click="showPwd"><svg-icon icon-class="eye"/></span>
+          </el-form-item>
+        </el-tab-pane>
+
+        <el-tab-pane label="手机号登录" name="second">
+          <el-form-item prop="username">
+            <el-input prefix-icon="el-icon-mobile-phone" name="username" type="text" v-model="loginForm.username" autoComplete="on" placeholder="手机号"/>
+          </el-form-item>
+          <el-form-item prop="password">
+            <el-input  class="message-code" prefix-icon="el-icon-message" name="password" :type="pwdType" @keyup.enter.native="handleLogin" v-model="loginForm.password"
+                      autoComplete="on"
+                      placeholder="验证码"></el-input>
+            <el-button style="display: inline-block" type="default">
+              获取验证码
+            </el-button>
+          </el-form-item>
+        </el-tab-pane>
+      </el-tabs>
+      <el-form-item>
+        <el-button type="primary" style="width:100%;" :loading="loading" @click.native.prevent="handleLogin">
+          登 录
+        </el-button>
+      </el-form-item>
+      <div style="text-align: left;margin-top: 24px;line-height: 22px;font-size: 14px;color: rgba(0,0,0,.65);">
+        其他登录方式<svg-icon icon-class="wechat"/>
+      </div>
+    </el-form>
+
+
+   <!-- <div style="width: 420px;height:450px; position: absolute;
     top: 0px;
     left: 0px;
     right: 0px;
     bottom: 0px;
-    margin: auto;background-color: #fff;
-    box-shadow: 0 2px 10px #999;border-radius: 4px;">
-    <iframe  style="width: 100%;height: 100%;display: block" key="tag"
-            :src="weixinUrl" frameborder="0"></iframe></div>
-    <social-sign />
+    margin: auto;
+    /*background-color: #fff;*/
+    /*box-shadow: 0 2px 10px #999;*/
+    /*border-radius: 4px;*/
+    ">
+    <iframe  style="width: 100%;height: 100%;display: block;background: transparent" key="tag"
+            :src="weixinUrl" frameborder="0"></iframe>
+    </div>-->
+
     <el-dialog title="第一次请绑定账号" :visible.sync="showDialog">
       <img :src="img">
       <input v-model="nickname"/>
@@ -92,10 +112,6 @@
       }
     },
     methods: {
-      hashCli() {
-
-        window.location.hash = new Date().getTime().toString()
-      },
       showPwd () {
         if (this.pwdType === 'password') {
           this.pwdType = ''
@@ -172,7 +188,7 @@
   $light_gray: #eee;
 
   .login-container {
-    position: fixed;
+    /*position: fixed;*/
     height: 100%;
     width: 100%;
     /*background-color: $bg;*/
@@ -182,44 +198,17 @@
       -webkit-box-shadow: 0 0 0px 1000px #293444 inset !important;
       -webkit-text-fill-color: #fff !important;
     }
-    input {
-      background: transparent;
-      border: 0px;
-      -webkit-appearance: none;
-      border-radius: 0px;
-      padding: 12px 5px 12px 15px;
-      color: $light_gray;
-      height: 47px;
-    }
-    .el-input {
-      display: inline-block;
-      height: 47px;
-      width: 85%;
-    }
-    .tips {
-      font-size: 14px;
-      color: #fff;
-      margin-bottom: 10px;
-    }
-    .svg-container {
-      padding: 6px 5px 6px 15px;
-      color: $dark_gray;
-      vertical-align: middle;
-      width: 30px;
-      display: inline-block;
-      &_login {
-        font-size: 20px;
-      }
-    }
     .title {
-      font-size: 26px;
-      font-weight: 400;
-      color: $light_gray;
+      font-size: 25px;
+      font-weight: 600;
+      color: #606266;
       margin: 0px auto 40px auto;
       text-align: center;
-      font-weight: bold;
     }
     .login-form {
+      background-color: #fff;
+      box-shadow: 0 2px 10px #999;
+      border-radius: 5px;
       position: absolute;
       left: 0;
       right: 0;
@@ -227,25 +216,29 @@
       padding: 35px 35px 15px 35px;
       margin: 120px auto;
     }
-    .el-form-item {
-      border: 1px solid rgba(255, 255, 255, 0.1);
-      background: rgba(0, 0, 0, 0.1);
-      border-radius: 5px;
-      color: #454545;
-    }
+
     .show-pwd {
       position: absolute;
       right: 10px;
-      top: 7px;
+      top: 0;
       font-size: 16px;
       color: $dark_gray;
       cursor: pointer;
       user-select: none;
     }
-    .thirdparty-button {
-      position: absolute;
-      right: 35px;
-      bottom: 28px;
-    }
+  }
+  .el-tabs__content .el-form-item {
+    margin-bottom: 24px;
+  }
+  .el-tabs__content .message-code {
+    width: calc(100% - 116px);
+  }
+  .svg-icon {
+    font-size: 24px;
+    margin-left: 16px;
+    vertical-align: middle !important;
+    color: rgba(0,0,0,.2);
+    cursor: pointer;
+    transition: color .3s;
   }
 </style>
